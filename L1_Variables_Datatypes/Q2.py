8b50a6b3d4edb972ca15e3a07298a18ce0d58e6a
Vighanesh Gavit <gavitvs0412@gmail.com>
2026-08-26 22:15:43 +0530

    Q2.py Added

---
+++ L1_Variables_Datatypes/Q2.py
@@ -0,0 +1,7 @@
+# Write a Program to input side of a square & print its area.
+
+sideOfSquare = float(input("Enter the side of square: "))
+
+areaOfSquare = sideOfSquare * sideOfSquare
+
+print("Area of Square: ", areaOfSquare)
