cf945b92e814c9103392aff800fc0f8d6e98e6f6
Vighanesh Gavit <gavitvs0412@gmail.com>
2026-08-26 22:21:30 +0530

    Q3.py Added

---
+++ L1_Variables_Datatypes/Q3.py
@@ -0,0 +1,8 @@
+# Write a Program to input 2 floating point numbers & print their average.
+
+num1 = float(input("Enter 1st Floating point number: "))
+num2 = float(input("Enter 2nd Floating point number: "))
+
+average = (num1 + num2) / 2
+
+print("Average of 2 Floating point numbers: ", average)
